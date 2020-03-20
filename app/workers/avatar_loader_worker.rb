b70d6f540d8f27649722546a02a25e9fa36a5c6d
omelinb <elros.ob@gmail.com>
2020-03-20 17:55:33 +0300

    Changed resize to crop

--- app/workers/avatar_loader_worker.rb
+++ app/workers/avatar_loader_worker.rb
@@ -4,6 +4,7 @@ class AvatarLoaderWorker
   include Sidekiq::Worker
 
   def perform(filepath)
-    MiniMagick::Image.new(filepath).resize '300x300'
+    # MiniMagick::Image.new(filepath).resize '300x300'
+    MiniMagick::Image.new(filepath).crop('300x300+0+0')
   end
 end
