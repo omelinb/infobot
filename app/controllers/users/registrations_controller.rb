f0d2a4676b7b05326f5e2785745b2875a7bf175f
omelinb <elros.ob@gmail.com>
2020-03-20 07:32:33 +0300

    Fix update controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,13 +21,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
-    AvatarAttachmentService.attach(@user, account_update_params[:avatar])
-
     respond_to do |format|
       if update_resource(@user, account_update_params.except(:avatar))
+        AvatarAttachmentService.attach(@user, account_update_params[:avatar])
+
         format.html { redirect_to root_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
