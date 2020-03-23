bb1eec87851475665aca67d8789b6376bb0959b3
omelinb <elros.ob@gmail.com>
2020-03-23 09:42:08 +0300

    Add User test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,13 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { '123456' }
     password_confirmation { '123456' }
+
+    transient do
+      avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/ruby.png')) }
+    end
+
+    after(:create) do |user, option|
+      user.avatar.attach(option.avatar)
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when updated' do
+    subject { create(:user).avatar }
+
+    it 'should to attach the image' do
+      is_expected.to be_an_instance_of(ActiveStorage::Attached::One)
+    end
+  end
 end
